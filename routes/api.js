1fe9e372c44fc947a3c208ab25580f05fc94d759
dionkb <dionkbaskara@gmail.com>
2023-05-04 01:15:59 -0400

    added read/write functionality to POST request

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@
 // const path = require('path'); May not need here now that I am making modular routes
 const apiRouter = require('express').Router();
 const dbNotes = require('../db/db.json');
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 // Creating a route for the api/notes pathway to read the db.json file and add it to the notes page when viewed
@@ -9,25 +10,29 @@ apiRouter.get('/notes', (req, res) => res.json(dbNotes));
 
 // Creating a POST request for the /api/notes pathway
 apiRouter.post('/notes', (req, res) => {
-    let response;
-    // Check if there is anything in the request body
-    if (req.body && req.body.title) {
-        // If there is no id property, give it one with the uuid package function
-        if (!req.body.id) {
-            req.body.id = uuidv4();
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
         };
-        // If all true, do the following
-        response = {
-        status: 'success',
-        data: req.body,
-        };
-        res.json(response.data.title + ` has been added!`);
-        console.info(response.data.title + ` has been added!`);
-    } 
-    // If above is false, do the following
-    else {
-        res.json('Request body must at least contain a title');
+        fs.readFile(`./db/db.json`, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                let savedNotes = JSON.parse(data);
+                savedNotes.push(newNote);
+                fs.writeFile(`./db/db.json`, JSON.stringify(savedNotes), (err) => {
+                    err ? console.error(err) : console.info(`Success!`)
+            });
+            }
+        });
     }
+    else {
+        console.log("You must include a title and text for the note")
+    };
 });
 
 // Allows these pathways to be imported by main server.js
